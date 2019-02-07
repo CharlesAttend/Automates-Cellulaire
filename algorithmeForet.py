d620ee4c0def5119d865894898de79b06f76713d
_Banji_ <38909289+TheBanji@users.noreply.github.com>
2019-02-07 08:20:04 +0100

    Add files via upload

---
+++ algorithmeForet.py
@@ -0,0 +1,34 @@
+﻿# -*- coding: utf-8 -*-
+
+from fenForet import getLargeurFen, getHauteurFen, getNbCellulesLargeur, getNbCellulesHauteur
+from genererForet import genererCsv
+
+def copieCsv():
+    i = 0
+    while(1):
+        ligneCsv = csvForet.readline()
+        if(ligneCsv == ""):
+            break
+        tmp = list(ligneCsv)
+        del tmp[-1]
+        for j in range(40):
+            valeur = int(tmp[j])
+            tabForet[i][j] = valeur
+        tmp.clear()
+        i += 1
+    csvForet.close()
+
+def getTabForet():
+    return tabForet
+
+largeurFen, hauteurFen = int(getLargeurFen()), int(getHauteurFen())
+nbCellulesLargeur, nbCellulesHauteur = int(getNbCellulesLargeur()), int(getNbCellulesHauteur())
+
+csvForet = open("foret.csv", "r")
+tabForet = []
+
+for i in range(40):
+    tabForet.append([0] * 40)
+
+genererCsv()
+copieCsv()
