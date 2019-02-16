bb0dc9f55e89cfcde73779eab45b9879ded421b6
Banji <38909289+TheBanji@users.noreply.github.com>
2019-02-16 15:42:22 +0100

    Update varCommunes.py

--- varCommunes.py
+++ varCommunes.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-class varGlobales(): # Classe permettant aux differents fichiers d'utiliser les memes 'variables'/constantes
-    def __init__(self): # Constructeur definissant toutes les 'attributs'/constantes de la classe
-        self.hauteurFenetre = 800 
+class varGlobales():  # Classe stockant quelques 'variables'/constantes pour eviter les conflits de valeurs entre les fichiers
+
+    def __init__(self):  # Constructeur permettant de definir les differents attributs de la classe
+        self.hauteurFenetre = 800
         self.largeurFenetre = 800
-        self.nbCellulesHauteur = 20
-        self.nbCellulesLargeur = 20 
-        self.nomCsv  = 'csv.csv'
-    
-    # Fonctions dites 'Accesseurs', elles retournent les différents attributs de la classe
-    
+        self.nbCellulesHauteur = 20  # Nombre de cellules en hauteur
+        self.nbCellulesLargeur = 20  # Nombre de cellules en largeur
+        self.nomCsv  = 'csv.csv'  # Nom du csv permettant la generation du csv
+
+    # Fonctions dites "Accesseurs", elles retournent les valeurs des differents attributs de la classe
+        
     def getHauteur(self):
         return self.hauteurFenetre
     def getLargeur(self):
