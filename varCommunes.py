41b8b24eef1db1b1af86c69c05513d119476e592
Banji <38909289+TheBanji@users.noreply.github.com>
2019-03-07 08:35:37 +0100

    Update varCommunes.py
    Ajouts de la possibilité de récupérer les chemins d'accès des textures

--- varCommunes.py
+++ varCommunes.py
@@ -2,15 +2,21 @@
 
 class varGlobales():  # Classe stockant quelques 'variables'/constantes pour eviter les conflits de valeurs entre les fichiers
 
-    def __init__(self):  # Constructeur permettant de definir les differents attributs de la classe
-        self.hauteurFenetre = 800
-        self.largeurFenetre = 800
-        self.nbCellulesHauteur = 20  # Nombre de cellules en hauteur
-        self.nbCellulesLargeur = 20  # Nombre de cellules en largeur
+    def __init__(self, hauteur, largeur, nbCellHauteur, nbCellLargeur):  # Constructeur permettant de definir les differents attributs de la classe
+        self.hauteurFenetre = hauteur
+        self.largeurFenetre = largeur
+        self.nbCellulesHauteur = nbCellHauteur  # Nombre de cellules en hauteur
+        self.nbCellulesLargeur = nbCellLargeur  # Nombre de cellules en largeur
         self.nomCsv  = 'csv.csv'  # Nom du csv permettant la generation du csv
+        # Retourne les différents nom des textures
+        self.textureArbre  = 'textures\arbre.png'
+        self.textureSol = 'textures\sol.png'
+        self.textureEau = 'textures\eau.png'
+        self.textureArbreBrule = 'textures\arbreBrule.png'
+        self.textureArbreEnFeu = 'textures\arbreEnFeu.png'
 
     # Fonctions dites "Accesseurs", elles retournent les valeurs des differents attributs de la classe
-        
+
     def getHauteur(self):
         return self.hauteurFenetre
     def getLargeur(self):
@@ -21,3 +27,5 @@ class varGlobales():  # Classe stockant quelques 'variables'/constantes pour evi
         return self.nbCellulesLargeur
     def getNomCsv(self):
         return self.nomCsv
+    def getTexturesName(self):
+        return(self.textureArbre, self.textureArbreBrule, self.textureArbreEnFeu, self.textureEau, self.textureSol)
