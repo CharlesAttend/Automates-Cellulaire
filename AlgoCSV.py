be3f050eeff0beb0bf3f4809e369b21fe23bc7c2
Charles <charlo22@hotmail.fr>
2019-03-07 09:51:58 +0100

    Add files via upload

--- AlgoCSV.py
+++ AlgoCSV.py
@@ -3,12 +3,26 @@ import csv
 from random import randint
 import classDialectCsv
 
+def transformationCsvListe():   # Fonction convertissant le csv en une liste a deux dimensions de nombres entiers
+    for i in range(10): # Nombre de cellules en hauteur
+        tmp = []
+        for j in list(csvForet.readline()):
+            if (j != "\n"):
+                tmp.append(int(j))
+        listeForet.append(tmp.copy())
+        tmp.clear()
+
 hauteur = 1000
 largeur = 1000
 tailleTx = 100
-with open("csv.csv", "w") as f:
-    #reader = csv.reader(f, dialect())
-    writer = csv.writer(f, classDialectCsv.Dialect)
+with open("csv.csv", "w", newline='') as f:
+    writer = csv.writer(f, classDialectCsv.Dialect())
     for i in range(hauteur//tailleTx):
         writer.writerow([randint(0,1) for j in range(largeur//tailleTx)])
-    f.close()
+
+with open("csv.csv", "r", newline='') as f:
+    reader = csv.reader(f, classDialectCsv.Dialect())
+    doubleList
+    for row in reader:
+
+        print(row[0])
