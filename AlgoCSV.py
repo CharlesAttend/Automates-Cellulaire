53d56ee94574ec0b08ec552d90e4a9fd65c89d7c
Charles <charlo22@hotmail.fr>
2019-03-07 09:26:16 +0100

    final csv generator
    final csv generator

--- AlgoCSV.py
+++ AlgoCSV.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 import csv
 from random import randint
-from varCommunes import *
-
-
 import classDialectCsv
-dialect = classDialectCsv.Dialect()
-hauteur = getHauteurFen()
-largeur = getLargeurFen()
-with open(getFileName(), "wb") as f:
+
+hauteur = 1000
+largeur = 1000
+tailleTx = 100
+with open("csv.csv", "w") as f:
     #reader = csv.reader(f, dialect())
     writer = csv.writer(f, classDialectCsv.Dialect)
-    for i in range(hauteur):
-        writer.writerow([randint(0,1) for j in range(largeur)])
+    for i in range(hauteur//tailleTx):
+        writer.writerow([randint(0,1) for j in range(largeur//tailleTx)])
     f.close()
