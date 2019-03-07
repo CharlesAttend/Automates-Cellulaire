ce71573591bafbc31d75546cebeb9d9af61d12b1
GugusV <47207723+GugusV@users.noreply.github.com>
2019-03-07 09:20:49 +0100

    Update projet_tkinter.py

--- projet_tkinter.py
+++ projet_tkinter.py
@@ -2,16 +2,26 @@ from tkinter import *
 from PIL import Image, ImageTk,ImageGrab
 import tkinter.messagebox
 import tkinter.filedialog
+import varCommunes
 
 
 
 def enregistrer():
-    x = canvas.winfo_rootx()
-    y = canvas.winfo_rooty()
-    w = canvas.winfo_width()
-    h = canvas.winfo_height()
+    x = Canevas.winfo_rootx()
+    y = Canevas.winfo_rooty()
+    w = Canevas.winfo_width()
+    h = Canevas.winfo_height()
     image=ImageGrab.grab((x+2, y+2, x+w-2, y+h-2))
     image.save("Feu.png")
+
+def dix () :
+    a = varCommunes.varGlobales(900, 900, 10)
+
+def cinquante () :
+    a = varCommunes.varGlobales(900, 900, 50)
+
+def cent () :
+    a = varCommunes.varGlobales(900, 900, 100)
 
 def sim_auto():
     pass
@@ -21,20 +31,24 @@ def pasapas():
 
 Fenetre = Tk()
 Fenetre.title("Image")
-Fenetre.geometry('1280x720')
-canvas = Canvas(Fenetre, width=1000, height=1000, background='yellow')
+Fenetre.geometry('1000x1000')
+canvas = Canvas(Fenetre, width=900, height=900, background='yellow')
 menubar = Menu(Fenetre)
 
-menufichier = Menu(menubar,tearoff=0)
-menufichier.add_command(label="Enregistrer l'image",command=enregistrer)
-menufichier.add_command(label="Quitter",command=Fenetre.destroy)
-menubar.add_cascade(label="Feu", menu=menufichier)
+menufichier = Menu(menubar, tearoff = 0)
+dimensions = Menu(menubar, tearoff = 0)
+menufichier.add_command(label = "Enregistrer l'image", command = enregistrer)
+menufichier.add_command(label = "Quitter", command = Fenetre.destroy)
+dimensions.add_command(label = "10x10", command = dix)
+dimensions.add_command(label = "50x50", command = cinquante)
+dimensions.add_command(label = "100x100", command = cent)
+menubar.add_cascade(label = "Fichier", menu = menufichier)
+menubar.add_cascade(label = "Dimensions", menu = dimensions)
 
 auto = Button(Fenetre, text = "Simulation Automatique", bg = "green", command = sim_auto)
 manuel = Button(Fenetre, text = "Simulation Pas à Pas", bg = "blue", command = pasapas)
 auto.grid(row = 0, column=0, sticky = "n")
 manuel.grid(row = 1, column = 0, sticky = "n")
-
 
 # Affichage du menu
 Fenetre.config(menu=menubar)
